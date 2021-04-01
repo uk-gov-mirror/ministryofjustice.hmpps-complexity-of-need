bdec38c2b9fdcd1f62d7634e01a80febfe1b1204
Ollie Treend <ollie@treend.com>
2021-04-01 15:22:36 +0100

    Remove redundant route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
 
   get "/ping" => "health#index"
   get "/health" => "health#index"
-  get "/release" => "health#release"
 
   # Default the request format to JSON – avoids need for .json file extension on paths
   defaults format: :json do

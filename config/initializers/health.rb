db5ae51372d7302ccf95d5648377474e1e6333f4
Jesus Laiz <zheileman@users.noreply.github.com>
2024-10-28 10:24:44 +0000

    MO-1581 Change DB health check (#125)
    Ticket: https://dsdmoj.atlassian.net/browse/MO-1581
    
    Ensures even after long periods of downtime or without requests, the DB check works. This was observed in non-prod envs that don't receive much traffic.

--- config/initializers/health.rb
+++ config/initializers/health.rb
@@ -8,7 +8,7 @@ config.x.init_epoch = Time.zone.now.to_i
 config.x.health_checks = Health.new(timeout_in_seconds_per_check: 2, num_retries_per_check: 2)
   .add_check(
     name: "db",
-    get_response: -> { ActiveRecord::Base.connection.active? },
+    get_response: -> { ActiveRecord::Base.connection.execute("SELECT 1").present? },
     check_response: ->(response) { response == true },
   )
   .add_check(
